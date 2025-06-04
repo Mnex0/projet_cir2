dc163c84fd114566b2f25b2ab358b40fafd6d285
Mnex0 <matthieu.head@gmail.com>
2025-06-04 10:38:15 +0200

    import_annee et moi fonctionnels

--- python/import.py
+++ python/import.py
@@ -1,71 +1,107 @@
-from claude import *
+import csv
+from io import StringIO
+
+def parse_csv(file_path):
+    liste = []
+    try:
+        with open(file_path, 'r', encoding='utf-8', newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                liste.append(row)
+        return liste
+
+    except FileNotFoundError:
+        print(f"Erreur : Le fichier {file_path} n'a pas été trouvé.")
+        return []
+    except Exception as e:
+        print(f"Erreur lors de la lecture du fichier : {e}")
+        return []
 
 def calculer_nb_lignes(nom_fichier) :
     with open(nom_fichier,"r") as fichier:
         l=fichier.readlines()
     return len(l)
 
-#def import_annee(nom_fichier_source, nom_fichier_destination):
-#    res = []
-#    n=calculer_nb_lignes("data.csv")
-#    with open(nom_fichier_source,"r") as fichier:
-#        fichier.readline()
-#        for i in range(1, n):
-#            temp = fichier.readline().split(',')
-#            res.append(temp[3][1:-1])
-#    #print(res)
-#    with open(nom_fichier_destination, "w") as fichier:
-#        for i in range(1,len(res)):
-#            fichier.write("INSERT INTO `annee` VALUES ("+res[i]+")\n")
+"""
+Toutes les fonctions commençant par 'import' - sauf pour 'mois' étant donnée la facilité du peuplement de cette dernière -
+prennent en argument une liste de liste correspondant au fichier csv parsé et un nom de fichier d'écriture intitulé 'fichier_destination', 
+écrivent à la fin du fichier destination les requêtes sql nécessaires pour peupler la database, à savoir uniquement des INSERT
+et ne renvoient rien.
+"""
 
-def import_installation(nom_fichier_source, nom_fichier_destination):
-    temp = parse_csv_file(nom_fichier_source)
+# ------------------------------- IMPORTS -----------------------------------------------
+
+def import_installation(liste, fichier_destination):
     res = []
-    for i in range(1, len(temp)):
+    for i in range(1, len(liste)):
         res.append([
-            int(temp[i][0]),                                        #id
-            ('NULL' if temp[i][4]=='' else int(temp[i][4])),        #nb_panneau
-            ('NULL' if temp[i][7]=='' else int(temp[i][7])),        #nb_ondulateur
-            ('NULL' if temp[i][10]=='' else int(temp[i][10])),      #puiss_crete
-            ('NULL' if temp[i][11]=='' else float(temp[i][11])),    #surface
-            ('NULL' if temp[i][12]=='' else int(temp[i][12])),      #pente
-            ('NULL' if temp[i][13]=='' else int(temp[i][13])),      #pente opti
-            ('NULL' if temp[i][14]=='' else temp[i][14]),           #orientation (varchar)
-            ('NULL' if temp[i][15]=='' else temp[i][15]),           #orientation opti (varchar)
-            ('NULL' if temp[i][17]=='' else int(temp[i][17])),      #produ pvgis
-            float(temp[i][18]),                                     #lat
-            float(temp[i][19]),                                     #lon
-            ('NULL' if temp[i][6]=='' else temp[i][6]),             #panneau modele
-            ('NULL' if temp[i][9]=='' else temp[i][9]),             #ondu modele
-            ('NULL' if temp[i][5]=='' else temp[i][5]),             #panneau marque
-            ('NULL' if temp[i][8]=='' else temp[i][8]),             #ondu marque
-            ('NULL' if temp[i][16]=='' else temp[i][16]),           #installateur
-            ('NULL' if temp[i][21]=='' else int(temp[i][21])),      #code insee
-            ('NULL' if temp[i][2]=='' else int(temp[i][2])),        #mois
-            ('NULL' if temp[i][3]=='' else int(temp[i][3]))         #annee
+            int(liste[i][0]),                                         # id (not NULL)
+            ('NULL' if liste[i][4]=='' else int(liste[i][4])),        # nb_panneau
+            ('NULL' if liste[i][7]=='' else int(liste[i][7])),        # nb_ondulateur
+            ('NULL' if liste[i][10]=='' else int(liste[i][10])),      # puissance_crete
+            ('NULL' if liste[i][11]=='' else float(liste[i][11])),    # surface
+            ('NULL' if liste[i][12]=='' else int(liste[i][12])),      # pente
+            ('NULL' if liste[i][13]=='' else int(liste[i][13])),      # pente_optimum
+            ('NULL' if liste[i][14]=='' else liste[i][14]),           # orientation (varchar)
+            ('NULL' if liste[i][15]=='' else liste[i][15]),           # orientation_optimum (varchar)
+            ('NULL' if liste[i][17]=='' else int(liste[i][17])),      # production_pvgis
+            float(liste[i][18]),                                      # lat (not NULL)
+            float(liste[i][19]),                                      # lon (not NULL)
+            ('NULL' if liste[i][6]=='' else liste[i][6]),             # modele_panneau
+            ('NULL' if liste[i][9]=='' else liste[i][9]),             # modele_ondulateur
+            ('NULL' if liste[i][16]=='' else liste[i][16]),           # nom_installateur
+            ('NULL' if liste[i][21]=='' else int(liste[i][21])),      # code insee
+            ('NULL' if liste[i][2]=='' else int(liste[i][2])),        # num_mois
+            ('NULL' if liste[i][3]=='' else int(liste[i][3]))         # num_annee
             ])
     print(res)
-    with open(nom_fichier_destination, "w") as fichier:
+    with open(fichier_destination, "a") as fichier:
+        line = ""
         for i in range(len(res)):
+            line = f"INSERT INTO `installation` VALUES ('{res[i][0]}')\n"
             fichier.write("INSERT INTO `installation` VALUES ('"+str(res[i][0])+"','"+str(res[i][1])+"','"+str(res[i][2])+"','"+str(res[i][3])+"','"+str(res[i][4])+"','"+str(res[i][5])+"','"+str(res[i][6])+"','"+str(res[i][7])+"','"+str(res[i][8])+"','"+str(res[i][9])+"','"+str(res[i][10])+"','"+str(res[i][11])+"','"+str(res[i][12])+"','"+str(res[i][13])+"','"+str(res[i][14])+"','"+str(res[i][15])+"','"+str(res[i][16])+"','"+str(res[i][17])+"','"+str(res[i][18])+"','"+str(res[i][19])+"')\n")
     return
 
-def import_annee(temp):
-    result = ""
-    for i in range(len(temp)):
-        if not str(temp[i][3]) in result:
-            result += "INSERT INTO `annee` VALUES ('" + str(temp[i][3]) + "')\n"
 
-def import_mois(temp):
-    result = ""
-    for i in range(len(temp)):
-        if not str(temp[i][2]) in result:
-            result += "INSERT INTO `mois` VALUES ('" + str(temp[i][2]) + "')\n"
+def import_annee(liste, fichier_destination):    
+    with open(fichier_destination, "a") as fichier:
+        line = ""
+        done = []
+        for i in range(1, len(liste)):
+            print(liste[i][3], (liste[i][3] in done))
+            if not(liste[i][3] in done): # liste[i][3] est l'annee de l'installation
+                line = f"INSERT INTO `annee` VALUES ('{liste[i][3]}')\n"
+                fichier.write(line)
+                done.append(liste[i][3])
 
-def import_localisation(temp):
-    result = ""
-    for i in range(len(temp)):
-        if not str(temp[i][24]) in result:
-            result += "INSERT INTO `region` VALUES ('" + str(temp[i][25]) + "')\nINSERT INTO `departement` VALUES ('" + (str(temp[i][21]))[:2]+str(temp[i][26])+str(temp[i][25])+"')\nINSERT INTO `ville` VALUES ('" + str(temp[i][]) + "')\nINSERT INTO `ville` VALUES ('" + str(temp[i][24]) + "')\n"
 
-import_installation("data.csv","sql2")
+def import_mois(fichier_destination):
+    with open(fichier_destination, "a") as fichier:
+        line = ""
+        for i in range(12):
+            line = f"INSERT INTO `mois` VALUES ('{i+1}')\n"
+            fichier.write(line)
+
+"""def import_localisation(liste, fichier_destination):
+    line = ""
+    for i in range(len(liste)):
+        if not str(liste[i][24]) in result:
+            line = "INSERT INTO `region` VALUES ('" + str(liste[i][25]) + "')\nINSERT INTO `departement` VALUES ('" + (str(liste[i][21]))[:2]+str(liste[i][26])+str(liste[i][25])+"')\nINSERT INTO `ville` VALUES ('" + str(liste[i][]) + "')\nINSERT INTO `ville` VALUES ('" + str(liste[i][24]) + "')\n"
+
+"""
+
+def communes(liste, fichier_destination):
+    # Cette fonction extrait les données du fichier "communes-france-2024-limite.csv"
+    # et renvoie une liste utilisable par les tables ayant besoin de code INSEE
+    line = ""
+
+# ------------------------------- Main --------------------------------------------------
+
+def main():
+    #communes = parse_csv("/dataRessources/communes-france-2024-limite.csv")
+    liste = parse_csv("dataRessources/cleanData.csv")
+    import_mois("import_mois.sql")
+    import_annee(liste, "import_annee.sql")
+    #import_installation("data.csv","sql2")
+
+main()

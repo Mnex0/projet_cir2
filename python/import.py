7e15698c99ac513736bbb09556ddd5da6de77832
Mnex0 <matthieu.head@gmail.com>
2025-06-05 08:22:25 +0200

    69

--- python/import.py
+++ python/import.py
@@ -1,13 +1,13 @@
 import csv
 
 def parse_csv(file_path):
-    liste = []
+    data = []
     try:
         with open(file_path, 'r', encoding='utf-8', newline='') as csvfile:
             reader = csv.reader(csvfile)
             for row in reader:
-                liste.append(row)
-        return liste
+                data.append(row)
+        return data
 
     except FileNotFoundError:
         print(f"Erreur : Le fichier {file_path} n'a pas été trouvé.")
@@ -24,14 +24,10 @@ def calculer_nb_lignes(nom_fichier) :
 def associe_INSEE(communes: list, ville, code_postal):
     for i in range(len(communes)):
         if (communes[i][6] == code_postal and communes[i][1] == ville):
-            #print(f"Trouvé")
             return communes[i][0]
-    #print(f"Code Insee non trouvé pour : {ville} - {code_postal}")
-    return f"p{code_postal}" # Renvoie un code insee de format "p" + Code_postal pour tout de même stocker l'information postale
+    return f"p{code_postal}"        # Renvoie un code insee de format "p" + Code_postal pour tout de même stocker l'information postale
 
-
-
-# ------------------------------- IMPORTS -----------------------------------------------
+# ___________________________ IMPORTS __________________________________________________________________________________________________________
 """
 Toutes les fonctions commençant par 'import' - sauf pour 'mois' étant donnée la facilité du peuplement de cette dernière -
 prennent en argument une liste de liste correspondant au fichier csv parsé et un nom de fichier d'écriture intitulé 'fichier_destination', 
@@ -39,114 +35,80 @@ prennent en argument une liste de liste correspondant au fichier csv parsé et u
 et ne renvoient rien.
 """
 
-def import_installation(data: list, communes: list, fichier_destination = "import_installation.sql"):
-    res = [] # Liste résultat
-    unique = [] # liste qui va contenir la liste de toutes les infos d'une ligne
-    for i in range(1, len(data)): # On démarre à 1 pour ne pas avoir l'en-tête
-        insee = associe_INSEE(communes, data[i][24], data[i][21])   # respectivement la localité et le code postal
-        temp = [
-            int(data[i][0]),                                        # id (not NULL)
-            ('NULL' if data[i][4]=='' else int(data[i][4])),        # nb_panneau
-            ('NULL' if data[i][7]=='' else int(data[i][7])),        # nb_ondulateur
-            ('NULL' if data[i][10]=='' else int(data[i][10])),      # puissance_crete
-            ('NULL' if data[i][11]=='' else float(data[i][11])),    # surface
-            ('NULL' if data[i][12]=='' else int(data[i][12])),      # pente
-            ('NULL' if data[i][13]=='' else int(data[i][13])),      # pente_optimum
-            ('NULL' if data[i][14]=='' else data[i][14]),           # orientation (varchar)
-            ('NULL' if data[i][15]=='' else data[i][15]),           # orientation_optimum (varchar)
-            ('NULL' if data[i][17]=='' else int(data[i][17])),      # production_pvgis
-            float(data[i][18]),                                     # lat (not NULL)
-            float(data[i][19]),                                     # lon (not NULL)
-            ('NULL' if data[i][6]=='' else data[i][6]),             # modele_panneau
-            ('NULL' if data[i][9]=='' else data[i][9]),             # modele_ondulateur
-            ('NULL' if data[i][16]=='' else data[i][16]),           # nom_installateur
-            ('NULL' if data[i][21]=='' else insee),                 # code insee
-            ('NULL' if data[i][2]=='' else int(data[i][2])),        # num_mois
-            ('NULL' if data[i][3]=='' else int(data[i][3]))         # num_annee
-        ]
-        if not(temp[1:] in unique):                                 # On vérifie si les données privées de l'id sont les mêmes pour ne pas ajouter les doublons
-            res.append(temp)                                        # On ajoute dans la liste avec l'id
-            unique.append(temp[1:])                                 # On ajoute sans l'id car, lui, diffère toujours et ne nous permet pas de trier les doublons
-    print(unique)
-    with open(fichier_destination, "w") as fichier:
-        fichier.write("-- _______________________ Installations __________________________--\n")
-        line = ""
-        for i in range(len(res)):
-            line = f'INSERT INTO `installation`(`id`, `nb_panneau`, `nb_onduleur`, `puissance_crete`, `surface`, `pente`, `pente_optimum`, `orientation`, `orientation_optimum`, `production_pvgis`, `lat`, `lon`, `modele_panneau`, `modele_onduleur`, `nom`, `code_INSEE`, `num_mois`, `num_annee`) VALUES ({res[i][0]}, {res[i][1]}, {res[i][2]}, {res[i][3]}, {res[i][4]}, {res[i][5]}, {res[i][6]}, "{res[i][7]}", "{res[i][8]}", {res[i][9]}, {res[i][10]}, {res[i][11]}, "{res[i][12]}", "{res[i][13]}", "{res[i][14]}", "{res[i][15]}", {res[i][16]}, {res[i][17]})\n'
-            fichier.write(line)
-
-# --------------------------- Premier import ------------------------
+# --------------------------- Premiers imports ----------------------------------------------------------------------------------
 """
-elif len(done)%10 == 0:
-    line = f",\n    ({liste[i][3]})
+Ici se trouvent la définition des fonction appelées lors de la création du premier import.
+Ce sont celles qui crééent les données des tables ne contenant aucune clés étrangères.
+Il s'agit donc ici des tables "annee", "installateur", "marque_ondu", "marque_pan", "mois" et "region"
+Cette séparation des imports permet de savoir si le problème vient des clés étrangères
 """
 
-def import_annee(liste: list, fichier_destination = "sql/import_annee.sql"):    
+def import_annee(data: list, fichier_destination = "sql/import_annee.sql"):    
     with open(fichier_destination, "w", encoding="utf-8") as fichier:           # En utf-8 pour éviter tout problème d'accents
         fichier.write("use projet_cir2;\n-- _______________________ Annees __________________________--\n")
         fichier.write("INSERT INTO `annee`(`num_annee`)\nVALUES")
         line = ""
         done = [""]
-        for i in range(1, len(liste)):
-            if not(liste[i][3] in done):                # liste[i][3] est l'annee de l'installation
-                line = f", ({liste[i][3]})"
+        for i in range(1, len(data)):
+            if not(data[i][3] in done):                # data[i][3] est l'annee de l'installation
+                line = f", ({data[i][3]})"
                 if len(done) == 1:
-                    line = f"\n    ({liste[i][3]})"     # Pas de début avec ","
+                    line = f"\n    ({data[i][3]})"     # Pas de début avec ","
                 elif len(done)%10 == 0:                 # Pour éviter les gros blocs sur une ligne, on ajoute un "\n"
-                    line = f",\n    ({liste[i][3]})"    # Ici on utilise la valeur sans "" car il s'agit d'un int, à l'avenir on utilisera f'("VALEUR")' pour éviter les problèmes d'apostrophes
+                    line = f",\n    ({data[i][3]})"    # Ici on utilise la valeur sans "" car il s'agit d'un int, à l'avenir on utilisera f'("VALEUR")' pour éviter les problèmes d'apostrophes
                 fichier.write(line)
-                done.append(liste[i][3])
+                done.append(data[i][3])
         fichier.write(";\n")
 
-def import_installateur(liste: list, fichier_destination = "sql/import_installateur.sql"):
+def import_installateur(data: list, fichier_destination = "sql/import_installateur.sql"):
     with open(fichier_destination, "a", encoding="utf-8") as fichier:
         fichier.write("-- _______________________ Installateurs __________________________--\n")
         fichier.write("INSERT INTO `installateur`(`nom`)\nVALUES")
         line = ""
         done = [""]
-        for i in range(1, len(liste)):
-            if not((liste[i][16]).lower() in done):     # liste[i][16] est le nom de l'installateure et lower permet de vérifier les doublons indistinctement des majuscules
-                line = f', ("{liste[i][16]}")'
+        for i in range(1, len(data)):
+            if not((data[i][16]).lower() in done):     # data[i][16] est le nom de l'installateure et lower permet de vérifier les doublons indistinctement des majuscules
+                line = f', ("{data[i][16]}")'
                 if len(done) == 1:
-                    line = f'\n    ("{liste[i][16]}")'
+                    line = f'\n    ("{data[i][16]}")'
                 elif len(done)%10 == 0:
-                    line = f',\n    ("{liste[i][16]}")'
+                    line = f',\n    ("{data[i][16]}")'
                 fichier.write(line)
-                done.append((liste[i][16]).lower())
+                done.append((data[i][16]).lower())
         fichier.write(";\n")
 
-def import_marque_ondu(liste: list, fichier_destination = "sql/import_marque_ondu.sql"):
+def import_marque_ondu(data: list, fichier_destination = "sql/import_marque_ondu.sql"):
     with open(fichier_destination, "a", encoding="utf-8") as fichier:
         fichier.write("-- _______________________ Marques Onduleurs __________________________--\n")
         fichier.write("INSERT INTO `marque_ondu`(`marque`)\nVALUES")
         line = ""
         done = [""]
-        for i in range(1, len(liste)):
-            if not((liste[i][8]).lower() in done): # liste[i][8] est la marque d'onduleur
-                line = f', ("{liste[i][8]}")'
+        for i in range(1, len(data)):
+            if not((data[i][8]).lower() in done): # data[i][8] est la marque d'onduleur
+                line = f', ("{data[i][8]}")'
                 if len(done) == 1:
-                    line = f'\n    ("{liste[i][8]}")'
+                    line = f'\n    ("{data[i][8]}")'
                 elif len(done)%10 == 0: # Pour éviter les gros blocs sur une ligne, on ajoute un "\n"
-                    line = f',\n    ("{liste[i][8]}")'
+                    line = f',\n    ("{data[i][8]}")'
                 fichier.write(line)
-                done.append((liste[i][8]).lower())
+                done.append((data[i][8]).lower())
         fichier.write(";\n")
 
-def import_marque_pan(liste: list, fichier_destination = "sql/import_marque_pan.sql"):
+def import_marque_pan(data: list, fichier_destination = "sql/import_marque_pan.sql"):
     with open(fichier_destination, "a", encoding="utf-8") as fichier:
         fichier.write("-- _______________________ Marques Panneaux __________________________--\n")
         fichier.write("INSERT INTO `marque_pan`(`marque`)\nVALUES")
         line = ""
         done = [""]
-        for i in range(1, len(liste)):
-            if not((liste[i][5]).lower() in done): # liste[i][5] est la marque de panneau
-                line = f', ("{liste[i][5]}")'
+        for i in range(1, len(data)):
+            if not((data[i][5]).lower() in done): # data[i][5] est la marque de panneau
+                line = f', ("{data[i][5]}")'
                 if len(done) == 1:
-                    line = f'\n    ("{liste[i][5]}")'
+                    line = f'\n    ("{data[i][5]}")'
                 elif len(done)%10 == 0: # Pour éviter les gros blocs sur une ligne, on ajoute un "\n"
-                    line = f',\n    ("{liste[i][5]}")'
+                    line = f',\n    ("{data[i][5]}")'
                 fichier.write(line)
-                done.append((liste[i][5]).lower())
+                done.append((data[i][5]).lower())
         fichier.write(";\n")
 
 def import_mois(fichier_destination = "sql/import_mois.sql"):
@@ -159,38 +121,89 @@ def import_mois(fichier_destination = "sql/import_mois.sql"):
             fichier.write(line)
         fichier.write(f" (12);\n")
 
-
-def import_region(liste: list, fichier_destination = "sql/import_region.sql"): # Attention, toujours revérifier s'il reste des caractères étranges dans le sql et les enlever
+def import_region(data: list, fichier_destination = "sql/import_region.sql"): # Attention, toujours revérifier s'il reste des caractères étranges dans le sql et les enlever
     with open(fichier_destination, "a", encoding="utf-8") as fichier:
         fichier.write("-- _______________________ Marques Regions __________________________--\n")
         fichier.write("INSERT INTO `region`(`nom_region`)\nVALUES")
         line = ""
         done = [""]
-        for i in range(1, len(liste)):
-            if not((liste[i][25]).lower() in done): # liste[i][25] est la région (administrative area lvl 1)
-                line = f', ("{liste[i][25]}")'
+        for i in range(1, len(data)):
+            if not((data[i][25]).lower() in done): # data[i][25] est la région (administrative area lvl 1)
+                line = f', ("{data[i][25]}")'
                 if len(done) == 1:
-                    line = f'\n    ("{liste[i][25]}")'
+                    line = f'\n    ("{data[i][25]}")'
                 elif len(done)%5 == 0:
-                    line = f',\n    ("{liste[i][25]}")'
+                    line = f',\n    ("{data[i][25]}")'
                 fichier.write(line)
-                done.append((liste[i][25]).lower())
+                done.append((data[i][25]).lower())
         fichier.write(";\n")
 
-# ------------------------------- Main --------------------------------------------------
+# --------------------------- Imports intermédiaires ----------------------------------------------------------------------------
+"""
+Ici se trouvent la définition des fonction appelées lors de la création de l'import intermédiare.
+Ce sont celles qui crééent les données des tables ne contenant aucune clés étrangères.
+"""
 
-def premierImport(liste: list, fichier_destination = "sql/premierImport.sql"):
-    import_annee(liste, fichier_destination)
-    import_installateur(liste, fichier_destination)
-    import_marque_ondu(liste, fichier_destination)
-    import_marque_pan(liste, fichier_destination)
+def import_departement(data: list, communes: list, fichier_destination = "import_departement.sql"):
+    with open(fichier_destination, "w", encoding="utf-8") as fichier:           # En utf-8 pour éviter tout problème d'accents
+        fichier.write("use projet_cir2;\n-- _______________________ Departements __________________________--\n")
+
+# --------------------------- Import final --------------------------------------------------------------------------------------
+
+def import_final_installation(data: list, communes: list, fichier_destination = "import_final_installation.sql"):
+    res = []  # Liste résultat
+    done = [] # liste qui va contenir la liste de toutes les infos d'une ligne
+    for i in range(1, len(data)): # On démarre à 1 pour ne pas avoir l'en-tête
+        insee = associe_INSEE(communes, data[i][24], data[i][21])   # respectivement la localité et le code postal
+        temp = [
+            int(data[i][0]),                                        # id (not NULL)
+            ('NULL' if data[i][4]=='' else int(data[i][4])),        # nb_panneau
+            ('NULL' if data[i][7]=='' else int(data[i][7])),        # nb_ondulateur
+            ('NULL' if data[i][10]=='' else int(data[i][10])),      # puissance_crete
+            ('NULL' if data[i][11]=='' else float(data[i][11])),    # surface
+            ('NULL' if data[i][12]=='' else int(data[i][12])),      # pente
+            ('NULL' if data[i][13]=='' else int(data[i][13])),      # pente_optimum
+            ('NULL' if data[i][14]=='' else data[i][14]),           # orientation (varchar car pouvant valoir 'Sud')
+            ('NULL' if data[i][15]=='' else data[i][15]),           # orientation_optimum (varchar aussi)
+            ('NULL' if data[i][17]=='' else int(data[i][17])),      # production_pvgis
+            float(data[i][18]),                                     # lat (not NULL)
+            float(data[i][19]),                                     # lon (not NULL)
+            ('NULL' if data[i][6]=='' else data[i][6]),             # modele_panneau
+            ('NULL' if data[i][9]=='' else data[i][9]),             # modele_ondulateur
+            ('NULL' if data[i][16]=='' else data[i][16]),           # nom_installateur
+            ('NULL' if data[i][21]=='' else insee),                 # code insee
+            ('NULL' if data[i][2]=='' else int(data[i][2])),        # num_mois
+            ('NULL' if data[i][3]=='' else int(data[i][3]))         # num_annee
+        ]
+        if not(temp[1:] in done):                     # On vérifie si les données privées de l'id sont les mêmes pour ne pas ajouter les doublons
+            res.append(temp)                          # On ajoute dans la liste avec l'id
+            done.append(temp[1:])                     # On ajoute sans l'id car, lui, diffère toujours et ne nous permet pas de trier les doublons
+    with open(fichier_destination, "w") as fichier:
+        fichier.write("-- _______________________ Installations __________________________--\n")
+        fichier.write("INSERT INTO `installation`(`id`, `nb_panneau`, `nb_onduleur`, `puissance_crete`, `surface`, `pente`, `pente_optimum`, `orientation`, `orientation_optimum`, `production_pvgis`, `lat`, `lon`, `modele_panneau`, `modele_onduleur`, `nom`, `code_INSEE`, `num_mois`, `num_annee`)\nVALUES\n")
+        line = ""
+        for i in range(len(res)):
+            line = f'''({res[i][0]}, {res[i][1]}, {res[i][2]}, {res[i][3]}, {res[i][4]}, {res[i][5]}, {res[i][6]}, "{res[i][7]}", "{res[i][8]}", {res[i][9]}, {res[i][10]}, {res[i][11]}, "{res[i][12]}", "{res[i][13]}", "{res[i][14]}", "{res[i][15]}", {res[i][16]}, {res[i][17]})\n'''
+            fichier.write(line)
+
+# _______________________________________________ Main _______________________________________________________________________________________________________________________
+
+def premier_import(data: list, fichier_destination = "sql/premier_import.sql"):
+    import_annee(data, fichier_destination)
+    import_installateur(data, fichier_destination)
+    import_marque_ondu(data, fichier_destination)
+    import_marque_pan(data, fichier_destination)
     import_mois(fichier_destination)
-    import_region(liste, fichier_destination)
+    import_region(data, fichier_destination)
+    print("Premier import SQL généré avec succès !\n")
+
+def import_intermediaire(data: list, fichier_destination = "sql/premier_import.sql"):
+    print("Import intermédiaire SQL généré avec succès !\n")
 
 def main():
-    liste = parse_csv("dataRessources/cleanData.csv")
+    data = parse_csv("dataRessources/cleanData.csv")
     communes = parse_csv("dataRessources/communes-france-2024-limite.csv")
-    premierImport(liste)
-    #import_installation(liste, communes, "sql/import_installation.sql")
+    premier_import(data)
+    #import_final_installation(data, communes, "sql/import_final_installation.sql")
 
 main()
